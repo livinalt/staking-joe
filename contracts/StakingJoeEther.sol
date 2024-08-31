b3845d078e56f5678580d1477127abad5200d09a
Livinalt <livinalt@gmail.com>
2024-08-31 15:45:14 +0100

    updated StakingJoeEther.sol

--- contracts/StakingJoeEther.sol
+++ contracts/StakingJoeEther.sol
@@ -41,13 +41,33 @@ contract StakingJoeEther {
     }
 
 
-    function unStakeJoe() external {
+     function unStakeJoe() external {
+        require(hasStaked[msg.sender], "You don't have an active stake");
+
+        StakerAccount memory staker = stakers[msg.sender];
+        require(block.timestamp >= staker.stakingStartTime + 1 days, "The staking period has not ended");
+
+        uint256 stakedAmount = staker.stakedAmount;
+        uint256 reward = calculateReward(block.timestamp - staker.stakingStartTime);
+        uint256 totalReturn = stakedAmount + reward;
+
+        staker.stakedAmount = 0;
+        staker.reward = 0;
+        hasStaked[msg.sender] = false;
+
+        // This transfers the staked Ether and the reward back to the user
+        (bool success, ) = msg.sender.call{value: totalReturn}("");
+        require(success, "Transfer has failed");
+
+        emit TokenUnstaked(msg.sender, totalReturn);
+    }
+
+    function calculateReward(uint256 duration) public pure returns (uint256) {
+        uint256 yield = (10 * 1e18) / 100;
+        uint256 reward = yield * (duration / 1 days); 
+        return reward;
+    }
     
-    }
-
-    function calculateReward(uint256 duration) public pure returns (uint256){
-
-    }
 
     function checkBalance() external view returns (uint256) {
         return stakers[msg.sender].stakedAmount;
